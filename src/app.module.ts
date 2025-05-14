b8b02bcce4c36e61c063509e47473a8f2f2f7adb
iwant <53087594+1want@users.noreply.github.com>
2025-05-14 09:38:14 +0800

    。。。

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProductsModule } from './products/products.module';
-import { Product } from './products/entities/product.entity'; // 导入 Product 实体
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ProductsModule } from './products/products.module'
+import { Product } from './products/entities/product.entity' // 导入 Product 实体
 
+// brew services start postgresql 开启本地服务器
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -13,7 +14,7 @@ import { Product } from './products/entities/product.entity'; // 导入 Product 
       password: '123456', // 你的数据库密码
       database: 'postgres', // 你要连接的数据库名称
       entities: [Product], // 列出所有需要 TypeORM 管理的实体类
-      synchronize: true, // 开发环境下可以设为 true，它会自动根据实体创建数据库表（生产环境建议设为 false）
+      synchronize: true // 开发环境下可以设为 true，它会自动根据实体创建数据库表（生产环境建议设为 false）
       // 其他 TypeORM 配置选项...
       // 例如，如果你想使用连接池:
       // extra: {
@@ -21,7 +22,7 @@ import { Product } from './products/entities/product.entity'; // 导入 Product 
       // },
       // logging: true, // 可以在控制台打印 SQL 查询语句
     }),
-    ProductsModule, // 确保 ProductsModule 也被导入
-  ],
+    ProductsModule // 确保 ProductsModule 也被导入
+  ]
 })
 export class AppModule {}
